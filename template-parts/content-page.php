a80ecf4e300e062255dd8aa8cfb84e02c566f054
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-03-11 22:46:44 +0800

    adjust fonts on news and events block

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,7 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if (!is_home()): ?>
+	<?php if (!is_front_page()): ?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
