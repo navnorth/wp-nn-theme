10db0984f85aab79242708b83fe3c407b9498c0c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-08 02:54:38 +0800

    update menu button target in mobile

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 					?>
 				</a>
 			</div>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<?php
