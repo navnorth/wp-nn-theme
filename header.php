5ea394ec43623f15e12ff549416e81c8e50aa4b8
Jess Hathaway <hathaway.jess@gmail.com>
2019-04-16 12:20:57 -0700

    update header styling, add logo

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 					$custom_logo_id = get_theme_mod( 'custom_logo' );
 					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 					if ( has_custom_logo() ) {
-						echo '<img src="'. esc_url( $logo[0] ) .'" align="left">';
+						echo '<img class="header-logo" src="'. esc_url( $logo[0] ) .'" align="left">';
 					}
 					echo '<h1 class="blogname">'. get_bloginfo( 'name' ) .'</h1>';
 					?>
