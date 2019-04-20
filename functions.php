81c187fb29a1d0838fd7c5e1e8c9a3a4498a9a39
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-20 21:42:43 +0800

    adjust bootstrap id

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ add_action( 'widgets_init', 'wp_nn_theme_widgets_init' );
  */
 function wp_nn_theme_scripts() {
     
-        wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+        wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
         
 	wp_enqueue_style( 'wp_nn_theme-style', get_stylesheet_uri() );
         
@@ -136,7 +136,7 @@ function wp_nn_theme_scripts() {
 
 	wp_enqueue_script( 'wp_nn_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array());
         
-        wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ) );
+        wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ) );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
