1e2c7830afbc0639b460822271d75bf30c3e318e
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-22 22:58:14 +0800

    add wrapper to heading

--- front-page.php
+++ front-page.php
@@ -9,28 +9,30 @@ get_header();
 ?>
 <div id="main">
     <div id="tc-heading" class="heading">
-        <?php if (get_field('home_page_banner_image_1')): ?>
-        <div class="col-md-4 padding-none">
-            <img src="<?php echo the_field('home_page_banner_image_1'); ?>" class="head-banner" />
-        </div>
-        <?php endif; ?>
-        <?php if (get_field('home_page_banner_image_2')): ?>
-        <div class="col-md-4 padding-none">
-            <img src="<?php echo the_field('home_page_banner_image_2'); ?>" class="head-banner" />
-        </div>
-        <?php endif; ?>
-        <?php if (get_field('home_page_banner_image_3')): ?>
-        <div class="col-md-4 padding-none">
-            <img src="<?php echo the_field('home_page_banner_image_3'); ?>" class="head-banner" />
-        </div>
-        <?php endif; ?>
-        <div class="primary-title" id="banner-heading">
-            <?php if (get_field('home_page_primary_title')): ?>
-            <h1 id="banner-title"><?php echo the_field('home_page_primary_title'); ?></h1>
+        <div class="tc-heading-wrapper">
+            <?php if (get_field('home_page_banner_image_1')): ?>
+            <div class="col-md-4 padding-none">
+                <img src="<?php echo the_field('home_page_banner_image_1'); ?>" class="head-banner" />
+            </div>
             <?php endif; ?>
-            <?php if (get_field('home_page_tagline')): ?>
-            <h2 id="banner-subtitle"><?php echo the_field('home_page_tagline'); ?></h2>
+            <?php if (get_field('home_page_banner_image_2')): ?>
+            <div class="col-md-4 padding-none">
+                <img src="<?php echo the_field('home_page_banner_image_2'); ?>" class="head-banner" />
+            </div>
             <?php endif; ?>
+            <?php if (get_field('home_page_banner_image_3')): ?>
+            <div class="col-md-4 padding-none">
+                <img src="<?php echo the_field('home_page_banner_image_3'); ?>" class="head-banner" />
+            </div>
+            <?php endif; ?>
+            <div class="primary-title" id="banner-heading">
+                <?php if (get_field('home_page_primary_title')): ?>
+                <h1 id="banner-title"><?php echo the_field('home_page_primary_title'); ?></h1>
+                <?php endif; ?>
+                <?php if (get_field('home_page_tagline')): ?>
+                <h2 id="banner-subtitle"><?php echo the_field('home_page_tagline'); ?></h2>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </div><!-- #primary -->
