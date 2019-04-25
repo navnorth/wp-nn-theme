b369cb867f9de5f65010d9e6db8313d431646a5d
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-26 00:08:12 +0800

    set focus to search field when search form displays

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
     $('.tc-search-form').on("mouseover", function(){
         $(this).addClass('focus');
-        $('.tc-search-field').addClass("show");
+        $('.tc-search-field').addClass("show").focus();
     });
 });
