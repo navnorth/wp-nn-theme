f3e210830395a7f4c4efe02550311acd1c416b29
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-29 20:49:22 +0800

    update inquiry set page heading with banner images from home page ACF

--- page.php
+++ page.php
@@ -11,13 +11,40 @@
  *
  * @package wp_nn_theme
  */
-
+global $post; 
 get_header();
 $col = 12;
 if (is_active_sidebar('sidebar-1'))
 	$col = 8;
-?>
 
+$front_page_id = get_option('page_on_front');
+
+?>
+<div id="tc-heading" class="heading">
+	<div class="tc-heading-wrapper">
+	    <?php if (get_field('home_page_banner_image_1', $front_page_id)): ?>
+	    <div class="col-md-4 padding-none">
+		<div class="banner-overlay overlay-green"></div>
+		<img src="<?php echo the_field('home_page_banner_image_1', $front_page_id); ?>" class="head-banner" />
+	    </div>
+	    <?php endif; ?>
+	    <?php if (get_field('home_page_banner_image_2', $front_page_id)): ?>
+	    <div class="col-md-4 padding-none">
+		<div class="banner-overlay overlay-orange"></div>
+		<img src="<?php echo the_field('home_page_banner_image_2', $front_page_id); ?>" class="head-banner" />
+	    </div>
+	    <?php endif; ?>
+	    <?php if (get_field('home_page_banner_image_3', $front_page_id)): ?>
+	    <div class="col-md-4 padding-none">
+		<div class="banner-overlay overlay-pink"></div>
+		<img src="<?php echo the_field('home_page_banner_image_3', $front_page_id); ?>" class="head-banner" />
+	    </div>
+	    <?php endif; ?>
+	    <div class="primary-title" id="page-banner-heading">
+		<h1 id="page-banner-title"><?php echo get_the_title($post->ID); ?></h1>
+	    </div>
+	</div>
+</div>
 	<div id="primary" class="content-area col-md-<?php echo $col; ?>">
 		<main id="main" class="site-main">
 
