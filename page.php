9cae65ac3d27dc587a5a531e5c5ae5d967fa7319
Jess Hathaway <hathaway.jess@gmail.com>
2019-05-06 12:35:51 -0700

    inquiry set main page styling changes

--- page.php
+++ page.php
@@ -16,54 +16,27 @@ get_header();
 $col = 12;
 if (is_active_sidebar('sidebar-1'))
 	$col = 8;
-
-$front_page_id = get_option('page_on_front');
-
 ?>
-<div id="tc-heading" class="heading">
-	<div class="tc-heading-wrapper">
-	    <?php if (get_field('home_page_banner_image_1', $front_page_id)): ?>
-	    <div class="col-md-4 padding-none">
-		<div class="banner-overlay overlay-green"></div>
-		<div style="background-image: url(<?php echo the_field('home_page_banner_image_1', $front_page_id); ?>)" class="head-banner"></div>
-	    </div>
-	    <?php endif; ?>
-	    <?php if (get_field('home_page_banner_image_2', $front_page_id)): ?>
-	    <div class="col-md-4 padding-none">
-		<div class="banner-overlay overlay-orange"></div>
-		<div style="background-image: url(<?php echo the_field('home_page_banner_image_2', $front_page_id); ?>)" class="head-banner"></div>
-	    </div>
-	    <?php endif; ?>
-	    <?php if (get_field('home_page_banner_image_3', $front_page_id)): ?>
-	    <div class="col-md-4 padding-none">
-		<div class="banner-overlay overlay-pink"></div>
-		<div style="background-image: url(<?php echo the_field('home_page_banner_image_3', $front_page_id); ?>)" class="head-banner"></div>
-	    </div>
-	    <?php endif; ?>
-	    <div class="primary-title" id="page-banner-heading">
-		<h1 id="page-banner-title"><?php echo get_the_title($post->ID); ?></h1>
-	    </div>
-	</div>
-</div>
-	<div id="primary" class="content-area col-md-<?php echo $col; ?>">
-		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<div id="primary" class="content-area col-md-<?php echo $col; ?>">
+	<main id="main" class="site-main">
 
-			get_template_part( 'template-parts/content', 'page' );
+	<?php
+	while ( have_posts() ) :
+		the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		get_template_part( 'template-parts/content', 'page' );
 
-		endwhile; // End of the loop.
-		?>
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	endwhile; // End of the loop.
+	?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
