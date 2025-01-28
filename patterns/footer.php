833e118eb0478b9fc435eed786d751d7d7af5df3
Jones Solano <mjsolano12345@gmail.com>
2025-01-29 04:45:23 +0800

    NN-424 - reiterate updates

---
+++ patterns/footer.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Title: Footer with hero and 3 column
+ * Slug: navnorth/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Description: A footer section with hero section and 3 column
+ */
+?>
+<!-- wp:group {"className":"footer-container","style":{"spacing":{"padding":{"top":"128px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"background":{"backgroundImage":{"url":"http://codex.local/wp-content/uploads/2025/01/nn-logo-mark-white.svg","id":502,"source":"file","title":"nn-logo-mark-white"},"backgroundSize":"550px","backgroundAttachment":"scroll","backgroundPosition":"100% 30%","backgroundRepeat":"no-repeat"}},"backgroundColor":"cyan","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group footer-container has-base-color has-cyan-background-color has-text-color has-background has-link-color" style="padding-top:128px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"20px","bottom":"20px"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-top:20px;margin-bottom:20px;padding-top:0;padding-bottom:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"875px"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"style":{"spacing":{"margin":{"bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-bottom:20px"><!-- wp:heading {"className":"footer-hero-title","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<h2 class="wp-block-heading footer-hero-title has-base-color has-text-color has-link-color">Invest in the future of education</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-nn-yellow footer-button-contact-us"} -->
+<div class="wp-block-button is-style-nn-yellow footer-button-contact-us"><a class="wp-block-button__link wp-element-button" href="/contact-us">contact us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"80px","bottom":"0px"},"blockGap":{"top":"0px","left":"81px"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:80px;margin-bottom:0px"><!-- wp:column {"width":"19%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:19%"><!-- wp:group {"className":"footer-logo-social-container","style":{"dimensions":{"minHeight":""},"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"16px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","justifyContent":"left"}} -->
+<div class="wp-block-group footer-logo-social-container"><!-- wp:image {"lightbox":{"enabled":false},"id":480,"width":"232px","height":"auto","sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><a href="/"><img src="http://codex.local/wp-content/uploads/2025/01/Logo-Horizontal.svg" alt="" class="wp-image-480" style="width:232px;height:auto"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"className":"footer-social-section","style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group footer-social-section"><!-- wp:social-links {"iconColor":"base","iconColorValue":"#FFFFFF","size":"has-normal-icon-size","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://www.linkedin.com/company/navnorth","service":"linkedin"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:social-links {"iconColor":"cyan","iconColorValue":"#26A1C6","iconBackgroundColor":"base","iconBackgroundColorValue":"#FFFFFF","size":"has-small-icon-size"} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color has-icon-background-color"><!-- wp:social-link {"url":"https://www.facebook.com/navnorth","service":"x","label":""} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#FFFFFF","size":"has-normal-icon-size","className":"is-style-logos-only","style":{"layout":{"selfStretch":"fit","flexSize":null}},"backgroundColor":"cyan"} -->
+<ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only has-cyan-background-color has-background"><!-- wp:social-link {"url":"https://www.facebook.com/navnorth","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":484,"width":"101px","height":"auto","aspectRatio":"4.6","scale":"cover","sizeSlug":"full","linkDestination":"custom","className":"footer-muse-logo"} -->
+<figure class="wp-block-image size-full is-resized footer-muse-logo"><a href="https://museable.com/"><img src="http://codex.local/wp-content/uploads/2025/01/Museable-Logo.svg" alt="" class="wp-image-484" style="aspect-ratio:4.6;object-fit:cover;width:101px;height:auto"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:group {"className":"footer-navigation-section-container","style":{"spacing":{"blockGap":"32px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","verticalAlignment":"top"}} -->
+<div class="wp-block-group footer-navigation-section-container"><!-- wp:group {"style":{"spacing":{"blockGap":"24px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"className":"has-medium-font-size","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"body"} -->
+<h2 class="wp-block-heading has-medium-font-size has-base-color has-text-color has-link-color has-body-font-family" style="font-style:normal;font-weight:700">OUR WORK</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":490,"overlayMenu":"never","style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"18px"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"24px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"className":"has-medium-font-size","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"body"} -->
+<h2 class="wp-block-heading has-medium-font-size has-base-color has-text-color has-link-color has-body-font-family" style="font-style:normal;font-weight:700">NAVIGATION NORTH</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"18px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":491,"overlayMenu":"never","style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"0"}}}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"875px"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"align":"full","className":"footer-bottom","style":{"spacing":{"padding":{"top":"32px","bottom":"32px"}},"border":{"top":{"width":"1px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull footer-bottom" style="border-top-width:1px;padding-top:32px;padding-bottom:32px"><!-- wp:paragraph -->
+<p>© [current_year] Navigation North</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"className":"footer-bottom-link","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group footer-bottom-link"><!-- wp:navigation {"ref":461,"overlayMenu":"never","style":{"spacing":{"blockGap":"24px"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
